21099a555ff21b879b718a99959ea801fae70245
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-22 11:04:37 +0200

    Sketch connexions

--- Pub_ts/iris-front/src/App.tsx
+++ Pub_ts/iris-front/src/App.tsx
@@ -1,21 +1,29 @@
 import * as React from 'react';
 import './App.css';
 
+import ConnexionUtil from './connexion-util';
+import { Socketman } from './socketman';
+
 import logo from './-ex-res/raw_logo.png';
 
 class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo}/><h1 className="App-title">Welcome to Iris</h1>
-        </header>
-        <p className="App-intro">
-         Hello, this might be a work in progress!
-        </p>
-      </div>
-    );
-  }
+    state = { isLoaded: true };
+
+    componentDidMount() {
+        Socketman.commence();
+    }
+
+    render() {
+        const { isLoaded } = this.state;
+
+        <div><img src={logo} /></div>;
+
+        if (!isLoaded) {
+            return (<div>Loading...</div>);
+        }
+
+        return <ConnexionUtil />;
+    }
 }
 
 export default App;
---
+++ Pub_ts/iris-front/src/connexion-util.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { SocketStateInfo, Socketman } from './socketman';
+import * as Fbemit from 'fbemitter';
+import './App.css';
+
+class ConnexionUtil extends React.Component {
+    state = { connexion_state: Array<SocketStateInfo>() };
+
+    constructor(props:any) {
+        super(props);
+
+        this.state = {
+            connexion_state: Array<SocketStateInfo>()
+        };
+    }
+
+    socketManSubscription: Fbemit.EventSubscription;
+
+    componentDidMount() {
+        let self = this;
+        this.socketManSubscription = Socketman.Events_Socket_Change.addListener('any', function () { self.update_sockets() });
+        this.update_sockets();
+    }
+
+    componentWillUnmount() {
+        this.socketManSubscription.remove();
+    }
+
+    update_sockets() {
+        let socket_info = Socketman.get_socket_state_info();
+        console.log(socket_info);
+        this.setState({ connexion_state: socket_info });
+    }
+
+    render() {
+        const { connexion_state } = this.state;
+        
+        return (<div className="connexion-util">
+            {connexion_state.map(item =>
+                <ConnexionElem state_info={item} key={item.host_name} />
+            )}
+        </div>);
+
+        // 
+    }
+}
+
+interface IConnexionElem {
+    state_info: SocketStateInfo;
+}
+
+class ConnexionElem extends React.Component<IConnexionElem, object> {
+    render() {
+        const { state_info } = this.props;
+
+        let className = "connexion";
+        className += state_info.connected ? " connected" : " disconnected";
+
+        return (
+            <div className={className} >
+                <div><img src={state_info.host_icon} /></div>
+                <div className="name">{state_info.host_name}</div>
+            </div>
+        );
+    }
+}
+
+export default ConnexionUtil;
