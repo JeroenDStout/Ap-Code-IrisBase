8dadc573a6f0d658f31b8b8ebf54ef8f13ea96e9
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-24 04:55:25 +0200

    Update app to use streamarea / sidepanel

--- Pub_ts/iris-front/src/App.tsx
+++ Pub_ts/iris-front/src/App.tsx
@@ -1,28 +1,39 @@
 import * as React from 'react';
 import './-gen/App.css';
 
-import ConnexionUtil from './connexion-util';
+//import ConnexionUtil from './connexion-util';
 import { Socketman } from './socketman';
+import SidePanel from './side-panel';
+import StreamArea from './stream-area';
+import { StreamWrangler } from './stream-wrangler';
 
-import logo from './-ex-res/raw_logo.png';
+//import logo from './-ex-res/raw_logo.png';
 
 class App extends React.Component {
     state = { isLoaded: true };
 
     componentDidMount() {
         Socketman.commence();
+        StreamWrangler.commence();
+    }
+
+    render_side_panel() {
+        
     }
 
     render() {
         const { isLoaded } = this.state;
 
-        <div><img src={logo} /></div>;
-
         if (!isLoaded) {
             return (<div>Loading...</div>);
         }
 
-        return <ConnexionUtil />;
+        return (
+            <div>
+                <StreamArea />
+                <SidePanel />
+            </div>
+        );
     }
 }
 
