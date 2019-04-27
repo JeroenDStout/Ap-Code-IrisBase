7cec90eaee4c338bc359c041e3bcedaa97312904
Jeroen D Stout <jeroenstout@gmail.com>
2019-04-27 23:46:27 +0200

    Add basic object files

---
+++ Pubc/Iris Objects.h
@@ -0,0 +1,23 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+#pragma once
+
+#include "BlackRoot/Pubc/UUID.h"
+
+#include "ToolboxBase/Pubc/Base Environment.h"
+
+#include "IrisBase/Pubc/Interface Layouts.h"
+
+namespace IrisBack {
+namespace Objects {
+
+    struct Object {
+        using UUID = BlackRoot::Identify::UUID;
+
+
+    };
+
+}
+}
